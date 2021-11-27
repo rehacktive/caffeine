685c92ed3a8ac56c871a5820fe33af9f80a60534
aw4y <mobile@rehacktive.net>
2021-11-27 18:07:48 +0100

    add integration test for postgres

--- service/server_test.go
+++ service/server_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"testing"
 
@@ -34,13 +35,183 @@ var invalidJsonForSchema = `{
 	"firstName":"john"
 }`
 
-func setupCaffeineTest() (*TestingRouter, Database) {
-	mockDb := database.MemDatabase{}
-	mockDb.Init()
-	mockDb.Upsert(testNamespace, testKey, []byte(jsonPayload))
+var tests = []testCase{
+	{
+		name:                 "test home handler",
+		method:               http.MethodGet,
+		path:                 "/",
+		payload:              "",
+		expectedResponseCode: http.StatusOK,
+		expectedResponse:     `["ns1"]`,
+	},
+	{
+		name:                 "test namespace get",
+		method:               http.MethodGet,
+		path:                 "/ns/" + testNamespace,
+		payload:              "",
+		expectedResponseCode: http.StatusOK,
+		expectedResponse:     fmt.Sprintf(`[{"key":"%v","value":%v}]`, testKey, jsonPayload),
+	},
+	{
+		name:                 "test namespace get not existing",
+		method:               http.MethodGet,
+		path:                 "/ns/" + "not_existing_namespace",
+		payload:              "",
+		expectedResponseCode: http.StatusNotFound,
+		expectedResponse:     "",
+	},
+	{
+		name:                 "test namespace delete",
+		method:               http.MethodDelete,
+		path:                 "/ns/" + testNamespace,
+		payload:              "",
+		expectedResponseCode: http.StatusAccepted,
+		expectedResponse:     "{}",
+	},
+	{
+		name:                 "test namespace delete not existing",
+		method:               http.MethodDelete,
+		path:                 "/ns/" + "not_existing_namespace",
+		payload:              "",
+		expectedResponseCode: http.StatusNotFound,
+		expectedResponse:     "",
+	},
+	{
+		name:                 "test keyvalue post",
+		method:               http.MethodPost,
+		path:                 "/ns/test/1",
+		payload:              jsonPayload,
+		expectedResponseCode: http.StatusCreated,
+		expectedResponse:     "",
+		dbCheck: func(d Database) error {
+			value, err := d.Get("test", "1")
+			if err != nil {
+				return err
+			}
+			if string(value) != jsonPayload {
+				fmt.Errorf("Expected %v got %s", jsonPayload, string(value))
+			}
+			return nil
+		},
+	},
+	{
+		name:                 "test keyvalue post invalid json",
+		method:               http.MethodPost,
+		path:                 "/ns/test/1",
+		payload:              "{some bad data...",
+		expectedResponseCode: http.StatusBadRequest,
+		expectedResponse:     "",
+	},
+	{
+		name:                 "test keyvalue get",
+		method:               http.MethodGet,
+		path:                 "/ns/" + testNamespace + "/" + testKey,
+		payload:              "",
+		expectedResponseCode: http.StatusOK,
+		expectedResponse:     jsonPayload,
+	},
+	{
+		name:                 "test keyvalue get not existing",
+		method:               http.MethodGet,
+		path:                 "/ns/" + testNamespace + "/not_existing",
+		payload:              "",
+		expectedResponseCode: http.StatusNotFound,
+		expectedResponse:     "",
+	},
+	{
+		name:                 "test keyvalue delete",
+		method:               http.MethodDelete,
+		path:                 "/ns/" + testNamespace + "/" + testKey,
+		payload:              "",
+		expectedResponseCode: http.StatusAccepted,
+		expectedResponse:     "{}",
+	},
+	{
+		name:                 "test keyvalue delete not existing",
+		method:               http.MethodDelete,
+		path:                 "/ns/" + testNamespace + "/not_existing",
+		payload:              "",
+		expectedResponseCode: http.StatusNotFound,
+		expectedResponse:     "",
+	},
+	{
+		name:                 "test schema post",
+		method:               http.MethodPost,
+		path:                 "/schema/user",
+		payload:              getUserSchema(),
+		expectedResponseCode: http.StatusCreated,
+		expectedResponse:     "",
+		dbCheck: func(d Database) error {
+			value, err := d.Get("user_schema", SchemaId)
+			if err != nil {
+				return err
+			}
+			if string(value) != jsonPayload {
+				fmt.Errorf("Expected %v got %s", jsonPayload, string(value))
+			}
+			return nil
+		},
+	},
+	{
+		name:                 "test schema get",
+		method:               http.MethodGet,
+		path:                 "/schema/user",
+		payload:              getUserSchema(),
+		expectedResponseCode: http.StatusOK,
+		expectedResponse:     getUserSchema(),
+		beforeTest: func(d Database) {
+			d.Upsert("user"+SchemaId, SchemaId, []byte(getUserSchema()))
+		},
+	},
+	{
+		name:                 "test schema get not existing",
+		method:               http.MethodGet,
+		path:                 "/schema/not_existing",
+		payload:              "",
+		expectedResponseCode: http.StatusNotFound,
+		expectedResponse:     "",
+	},
+	{
+		name:                 "test schema delete",
+		method:               http.MethodDelete,
+		path:                 "/schema/user",
+		payload:              "",
+		expectedResponseCode: http.StatusAccepted,
+		expectedResponse:     "{}",
+		beforeTest: func(d Database) {
+			d.Upsert("user"+SchemaId, SchemaId, []byte(getUserSchema()))
+		},
+	},
+	{
+		name:                 "test post valid json with schema",
+		method:               http.MethodPost,
+		path:                 "/ns/user/1",
+		payload:              validJsonForSchema,
+		expectedResponseCode: http.StatusCreated,
+		expectedResponse:     validJsonForSchema,
+		beforeTest: func(d Database) {
+			d.Upsert("user"+SchemaId, SchemaId, []byte(getUserSchema()))
+		},
+	},
+	{
+		name:                 "test post invalid json with schema",
+		method:               http.MethodPost,
+		path:                 "/ns/user/1",
+		payload:              invalidJsonForSchema,
+		expectedResponseCode: http.StatusBadRequest,
+		expectedResponse:     `{ "status": 400, "message": "(root): lastName is required" }`,
+		beforeTest: func(d Database) {
+			d.Upsert("user"+SchemaId, SchemaId, []byte(getUserSchema()))
+		},
+	},
+}
+
+func setupCaffeineTest(db Database) *TestingRouter {
+	db.Init()
+	db.Upsert(testNamespace, testKey, []byte(jsonPayload))
 
 	server := Server{
-		db: &mockDb,
+		db: db,
 	}
 
 	testingRouter := TestingRouter{Router: mux.NewRouter()}
@@ -49,199 +220,39 @@ func setupCaffeineTest() (*TestingRouter, Database) {
 	testingRouter.AddHandler(KeyValuePattern, server.keyValueHandler)
 	testingRouter.AddHandler(SchemaPattern, server.schemaHandler)
 
-	return &testingRouter, &mockDb
+	return &testingRouter
 }
 
-func TestHandlers(t *testing.T) {
-	tests := []testCase{
-		{
-			name:                 "test home handler",
-			method:               http.MethodGet,
-			path:                 "/",
-			payload:              "",
-			expectedResponseCode: http.StatusOK,
-			expectedResponse:     `["ns1"]`,
-		},
-		{
-			name:                 "test namespace get",
-			method:               http.MethodGet,
-			path:                 "/ns/" + testNamespace,
-			payload:              "",
-			expectedResponseCode: http.StatusOK,
-			expectedResponse:     fmt.Sprintf(`[{"key":"%v","value":%v}]`, testKey, jsonPayload),
-		},
-		{
-			name:                 "test namespace get not existing",
-			method:               http.MethodGet,
-			path:                 "/ns/" + "not_existing_namespace",
-			payload:              "",
-			expectedResponseCode: http.StatusNotFound,
-			expectedResponse:     "",
-		},
-		{
-			name:                 "test namespace delete",
-			method:               http.MethodDelete,
-			path:                 "/ns/" + testNamespace,
-			payload:              "",
-			expectedResponseCode: http.StatusAccepted,
-			expectedResponse:     "{}",
-		},
-		{
-			name:                 "test namespace delete not existing",
-			method:               http.MethodDelete,
-			path:                 "/ns/" + "not_existing_namespace",
-			payload:              "",
-			expectedResponseCode: http.StatusNotFound,
-			expectedResponse:     "",
-		},
-		{
-			name:                 "test keyvalue post",
-			method:               http.MethodPost,
-			path:                 "/ns/test/1",
-			payload:              jsonPayload,
-			expectedResponseCode: http.StatusCreated,
-			expectedResponse:     "",
-			dbCheck: func(d Database) error {
-				value, err := d.Get("test", "1")
-				if err != nil {
-					return err
-				}
-				if string(value) != jsonPayload {
-					fmt.Errorf("Expected %v got %s", jsonPayload, string(value))
-				}
-				return nil
-			},
-		},
-		{
-			name:                 "test keyvalue post invalid json",
-			method:               http.MethodPost,
-			path:                 "/ns/test/1",
-			payload:              "{some bad data...",
-			expectedResponseCode: http.StatusBadRequest,
-			expectedResponse:     "",
-		},
-		{
-			name:                 "test keyvalue get",
-			method:               http.MethodGet,
-			path:                 "/ns/" + testNamespace + "/" + testKey,
-			payload:              "",
-			expectedResponseCode: http.StatusOK,
-			expectedResponse:     jsonPayload,
-		},
-		{
-			name:                 "test keyvalue get not existing",
-			method:               http.MethodGet,
-			path:                 "/ns/" + testNamespace + "/not_existing",
-			payload:              "",
-			expectedResponseCode: http.StatusNotFound,
-			expectedResponse:     "",
-		},
-		{
-			name:                 "test keyvalue delete",
-			method:               http.MethodDelete,
-			path:                 "/ns/" + testNamespace + "/" + testKey,
-			payload:              "",
-			expectedResponseCode: http.StatusAccepted,
-			expectedResponse:     "{}",
-		},
-		{
-			name:                 "test keyvalue delete not existing",
-			method:               http.MethodDelete,
-			path:                 "/ns/" + testNamespace + "/not_existing",
-			payload:              "",
-			expectedResponseCode: http.StatusNotFound,
-			expectedResponse:     "",
-		},
-		{
-			name:                 "test schema post",
-			method:               http.MethodPost,
-			path:                 "/schema/user",
-			payload:              getUserSchema(),
-			expectedResponseCode: http.StatusCreated,
-			expectedResponse:     "",
-			dbCheck: func(d Database) error {
-				value, err := d.Get("user_schema", SchemaId)
-				if err != nil {
-					return err
-				}
-				if string(value) != jsonPayload {
-					fmt.Errorf("Expected %v got %s", jsonPayload, string(value))
-				}
-				return nil
-			},
-		},
-		{
-			name:                 "test schema get",
-			method:               http.MethodGet,
-			path:                 "/schema/user",
-			payload:              getUserSchema(),
-			expectedResponseCode: http.StatusOK,
-			expectedResponse:     getUserSchema(),
-			beforeTest: func(d Database) {
-				d.Upsert("user"+SchemaId, SchemaId, []byte(getUserSchema()))
-			},
-		},
-		{
-			name:                 "test schema get not existing",
-			method:               http.MethodGet,
-			path:                 "/schema/not_existing",
-			payload:              "",
-			expectedResponseCode: http.StatusNotFound,
-			expectedResponse:     "",
-		},
-		{
-			name:                 "test schema delete",
-			method:               http.MethodDelete,
-			path:                 "/schema/user",
-			payload:              "",
-			expectedResponseCode: http.StatusAccepted,
-			expectedResponse:     "{}",
-			beforeTest: func(d Database) {
-				d.Upsert("user"+SchemaId, SchemaId, []byte(getUserSchema()))
-			},
-		},
-		{
-			name:                 "test post valid json with schema",
-			method:               http.MethodPost,
-			path:                 "/ns/user/1",
-			payload:              validJsonForSchema,
-			expectedResponseCode: http.StatusCreated,
-			expectedResponse:     validJsonForSchema,
-			beforeTest: func(d Database) {
-				d.Upsert("user"+SchemaId, SchemaId, []byte(getUserSchema()))
-			},
-		},
-		{
-			name:                 "test post invalid json with schema",
-			method:               http.MethodPost,
-			path:                 "/ns/user/1",
-			payload:              invalidJsonForSchema,
-			expectedResponseCode: http.StatusBadRequest,
-			expectedResponse:     `{ "status": 400, "message": "(root): lastName is required" }`,
-			beforeTest: func(d Database) {
-				d.Upsert("user"+SchemaId, SchemaId, []byte(getUserSchema()))
-			},
-		},
-	}
-
+func testHandlers(db Database, t *testing.T) {
 	for _, test := range tests {
-		testingRouter, mockDb := setupCaffeineTest()
+		testingRouter := setupCaffeineTest(db)
 		if test.beforeTest != nil {
-			test.beforeTest(mockDb)
+			test.beforeTest(db)
 		}
 		log.Println("running test: ", test.name)
 		req, _ := http.NewRequest(test.method, test.path, strings.NewReader(test.payload))
 		response := testingRouter.ExecuteRequest(req)
-		testingRouter.CheckResponseCode(t, test.expectedResponseCode, response.Code)
+		checkResponseCode(t, test.name, test.expectedResponseCode, response.Code)
 		if test.expectedResponse != "" {
-			testingRouter.CheckResponse(t, response.Body.String(), test.expectedResponse)
+			checkResponse(t, test.name, response.Body.String(), test.expectedResponse)
 		}
 		if test.dbCheck != nil {
-			err := test.dbCheck(mockDb)
+			err := test.dbCheck(db)
 			if err != nil {
 				t.Errorf(err.Error())
 			}
 		}
-
 	}
+}
+
+func Test_UnitTest_MemoryDb(t *testing.T) {
+	testHandlers(&database.MemDatabase{}, t)
+}
+
+func Test_UnitTest_StorageDb(t *testing.T) {
+	db := &database.StorageDatabase{
+		RootDirPath: "/tmp/caffeine_test1",
+	}
+	testHandlers(db, t)
+	os.RemoveAll("/tmp/caffeine_test1")
 }
